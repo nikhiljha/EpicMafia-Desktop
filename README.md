fbf008e5154dd6620a71f946f07cfa14919acca1
nikhiljha <nikhilfromirvine@gmail.com>
2016-03-08 16:46:33 -0800

    Make it

---
+++ README.md
@@ -0,0 +1,4 @@
+# EpicMafia-Desktop
+EpicMafia for Desktop (Windows, OSX, Mac) - written in node and electron.
+
+Self explanatory, use EpicMafia on Desktop.
---
+++ init.js
@@ -0,0 +1,40 @@
+// We are based on electron, so we need to initiate it.
+var electron = require('electron');
+var system = "Unknown OS";
+var os = require('os')
+
+switch(os.type()) {
+    case "Linux":
+        system = "Linux";
+        break;
+    case "Darwin":
+        system = "Mac OSX";
+        break;
+    case "Windows_NT":
+        system = "Windows";
+        break;
+    default:
+        system = "Unknown OS";
+}
+
+// When the app is ready...
+electron.app.on('ready', function () {
+  // Create the BrowserWindow variable.
+  const BrowserWindow = require('electron').BrowserWindow;
+
+  // Create a new window and let it load in the background.
+  var win = new BrowserWindow({width: 800, height: 600, show: false, title: "EpicMafia for " + system});
+
+  // When it is closed, delete the window.
+  win.on('closed', function() {
+    win = null;
+  });
+
+  // Don't let the page title change!
+  win.on('page-title-updated', function(event) {
+    event.preventDefault();
+  });
+
+  win.loadURL('https://epicmafia.com/lobby');
+  win.show();
+})
